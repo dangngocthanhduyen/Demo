240ede8fa1fabe93277d778a83f8325b019425de
dangngocthanhduyen <dangngocthanhduyen.2712@gmail.com>
2017-12-19 15:59:48 +0700

    Add soure code AbstractClass_Interface

---
+++ Zoo/Abstract/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abctract_Class
+{
+    abstract class Animal //Định nghĩa 1 lớp trừu tượng
+    {
+        public string strName;
+        public abstract void Sounding(); //Khai báo 1 phương thức trừu tượng bằng từ khoá abstract
+    }
+    class Duck : Animal //Định nghĩa lớp dẫn xuất hay gọi là kế thừa Animal
+    {
+        public override void Sounding()
+        {
+            Console.WriteLine("Cap cap cap...");
+        }
+    }
+    //Định nghĩa lớp Dẫn xuất Dog
+    class Dog : Animal
+    {
+        public override void Sounding()
+        {
+            Console.WriteLine("Gau gau guau...");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // class duck.
+            Duck objDuck = new Duck();
+            objDuck.strName = "Donald";
+            System.Console.WriteLine("Class Duck: {0}", objDuck.strName);
+            objDuck.Sounding();
+
+            //class dog. 
+            Dog objDog = new Dog();
+            objDog.strName = "Pluto";
+            System.Console.WriteLine("Class Dog: {0}", objDog.strName);
+            objDog.Sounding();
+
+            System.Console.ReadLine();
+        }
+    }
+}
---
+++ Zoo/Interface/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    public interface itfFly //khai báo interface cho việc bay
+    {
+        void Fly();
+    }
+    public interface itfRun //khai báo interface cho việc chạy
+    {
+        void Run();
+    }
+    public class classBird : itfFly, itfRun //Lớp classBird được thừa kế 2 interfaces itfFly, itfRun
+    {
+        public void Fly()
+        {
+            Console.WriteLine("Bird flies by 2 wings");
+        }
+        public void Run()
+        {
+            Console.WriteLine("Bird runs by 2 legs");
+        }
+    }
+    public class classPlane : itfFly, itfRun //Lớp classPlane thừa kế 2 interfaces itfFly, itfRun
+    {
+        public void Fly()
+        {
+            Console.WriteLine("The plane flies by engine");
+        }
+        public void Run()
+        {
+            Console.WriteLine("The plane runs by the wheel");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            classBird objBird = new classBird();
+            objBird.Fly();
+            objBird.Run();
+
+            classPlane objPlane = new classPlane();
+            objPlane.Fly();
+            objPlane.Run();
+
+            List < itfFly > listfly= new List<itfFly>();
+            listfly.Add(objBird);
+            listfly.Add(objPlane);
+            foreach(var item in listfly)
+            {
+                item.Fly();
+            }
+
+            System.Console.ReadLine();
+        }
+    }
+}
